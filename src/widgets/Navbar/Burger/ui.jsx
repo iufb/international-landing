f29b0a9aef449cf5284edd910f285c1173bf2632
iufb <misakilover228@gmail.com>
2024-06-14 14:24:48 +0500

    feat: burger

--- src/widgets/Navbar/Burger/ui.jsx
+++ src/widgets/Navbar/Burger/ui.jsx
@@ -1,12 +1,14 @@
 "use client";
 import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import { createPortal } from "react-dom";
+import { useTranslations } from "use-intl";
 import styles from "./ui.module.css";
 
 export const Burger = ({ className }) => {
-  const [opened, setOpened] = useState(false);
+  const [opened, setOpened] = useState(null);
   return (
     <>
       <Image
@@ -19,14 +21,98 @@ export const Burger = ({ className }) => {
       />
       {createPortal(
         <div
-          className={clsx(opened ? styles.container : styles["slide-out"])}
-          style={{}}
+          className={clsx(
+            styles.container,
+            opened == null && styles.hidden,
+            opened ? styles["slide-in"] : [styles["slide-out"]]
+          )}
         >
-          sf
-          <button onClick={() => setOpened(false)}>clsoe</button>
+          <Menu
+            closeIcon={
+              <Image
+                src={"/close.svg"}
+                width={48}
+                height={48}
+                alt="close"
+                className={styles.close}
+                onClick={() => setOpened(false)}
+              />
+            }
+          />
         </div>,
         document.body
       )}
     </>
+  );
+};
+
+const Menu = ({ closeIcon }) => {
+  const t = useTranslations("nav");
+  const [opened, setOpened] = useState(null);
+  const [link, setLink] = useState(null);
+  return (
+    <div className={styles.menu}>
+      {closeIcon}
+      {t.raw("links").map((link) => {
+        if (link.submenu == "") {
+          return <Link href={link.href}>{link.label}</Link>;
+        } else {
+          return (
+            <label className={styles.linkWithMenu}>
+              <span
+                data-name={link.submenu[0].label}
+                onClick={() => {
+                  setOpened(true);
+                  setLink(link);
+                }}
+              >
+                {link.label}
+              </span>
+              <Image
+                src="/arrow.svg"
+                alt="arrow"
+                width={26}
+                height={14}
+                className={styles.arrow}
+              />
+            </label>
+          );
+        }
+      })}
+      {link && (
+        <SecondLevel link={link} opened={opened} setOpened={setOpened} />
+      )}
+    </div>
+  );
+};
+const SecondLevel = ({ link, opened, setOpened }) => {
+  return createPortal(
+    <div
+      style={{
+        zIndex: 10,
+      }}
+      className={clsx(
+        styles.container,
+        opened == null && styles.hidden,
+        opened ? styles["slide-in"] : [styles["slide-out"]]
+      )}
+    >
+      <label className={styles.back} onClick={() => setOpened(false)}>
+        <Image
+          src="/arrow.svg"
+          alt="arrow"
+          width={26}
+          height={14}
+          style={{ transform: "rotate(-90deg)" }}
+        />{" "}
+        {link.label}
+      </label>
+      <div className={styles.menu}>
+        {link.submenu.map((link) => (
+          <Link href={link.href}>{link.label}</Link>
+        ))}
+      </div>
+    </div>,
+    document.body
   );
 };
